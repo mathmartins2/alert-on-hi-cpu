cb3698d84db66a3d488a99dd11e6f0db55a8b8dd
mathmartins2 <dev.sincroni@gmail.com>
2023-01-26 16:06:16 -0300

    v1

---
+++ index.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import os from 'os'
+
+const threshold = 90; 
+
+setInterval(async () => {
+  const usage = os.loadavg()[0];
+  const percentage = usage * 100;
+
+  if (percentage > threshold) {
+    await sendDiscordMessage(`CPU usage is at ${percentage}%, which is above the threshold of ${threshold}%.`);
+    console.log(`CPU usage is at ${percentage}%, which is above the threshold of ${threshold}%.`);
+  }
+}, 3000);
+
+
+const sendDiscordMessage = async (message) => {
+  await axios.post(process.env.DISCORD_URL, {
+    username: 'LULA CAMARADA',
+    content: message
+  })
+}
