17ed7c2c68870603e9155bbc5709f1178114ecd2
mathmartins2 <dev.sincroni@gmail.com>
2023-01-26 16:44:27 -0300

    math

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const threshold = 80;
 export default async function checkCPU() {
     setInterval(async () => {
         let { currentLoad: currentLoadSystem } = await si.currentLoad()
+        currentLoadSystem = Math.round(currentLoadSystem)
         console.log(`CPU usage is at ${currentLoadSystem}%, which is below the threshold of ${threshold}%.`);
         if (currentLoadSystem >= threshold && currentLoadSystem < 100) {
             sendDiscordMessage(`CPU usage is at ${currentLoadSystem}%, which is above the threshold of ${threshold}%.`)
