cafa9dc30abd052feb7accf2d20671bd00646973
mathmartins2 <dev.sincroni@gmail.com>
2023-01-26 16:53:24 -0300

    change

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ import 'dotenv/config'
 import si from 'systeminformation';
 import axios from 'axios';
 const threshold = 90;
+let lastNotificationTime = null;
 
 export default async function checkCPU() {
-    setInterval(async () => {
-        let { currentLoad: currentLoadSystem } = await si.currentLoad()
-        currentLoadSystem = Math.round(currentLoadSystem)
-        console.log(`CPU usage is at ${currentLoadSystem}%, which is below the threshold of ${threshold}%.`);
-        if (currentLoadSystem >= threshold) {
-            sendDiscordMessage(`CPU usage is at ${currentLoadSystem}%, which is above the threshold of ${threshold}%. CORRE Q O SGEDU TA PEGANDO FOGO`)
-        }
-    }, 3000);
+  setInterval(async () => {
+    let { currentLoad: currentLoadSystem } = await si.currentLoad();
+    currentLoadSystem = Math.round(currentLoadSystem);
+    console.log(`CPU usage is at ${currentLoadSystem}%, which is below the threshold of ${threshold}%.`);
+    if (currentLoadSystem >= threshold) {
+      const currentTime = Date.now();
+      if (!lastNotificationTime || currentTime - lastNotificationTime > 300000) {
+        sendDiscordMessage(`CPU usage is at ${currentLoadSystem}%, which is above the threshold of ${threshold}%. CORRE Q O SGEDU TA PEGANDO FOGO @everyone`);
+        lastNotificationTime = currentTime;
+      }
+    }
+  }, 3000);
 }
-checkCPU()
+checkCPU();
 
 const sendDiscordMessage = async (message) => {
   await axios.post(process.env.DISCORD_URL, {
     username: 'LULA CAMARADA',
     content: message
-  })
-}
+  });
+};
